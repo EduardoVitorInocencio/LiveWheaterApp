90124eeb787313f2c1589f1b7517ec03c39f74e5
Eduardo Inocencio <eduardo.inocencio@wp.numerator.com>
2026-08-19 23:58:15 -0300

    feat: Featching data from open-meteo API implementing getCoordinates(To get coordinates by name) and search ( to get the variables as temperature by coordinates).

--- src/components/WheatherApp.jsx
+++ src/components/WheatherApp.jsx
@@ -4,7 +4,61 @@ import rainy from '../assets/images/rainy.png'
 import snowy from '../assets/images/snowy.png'
 
 const WheatherApp = () => {
-  
+    // Busca latitude e longitude pelo nome da cidade
+    const getCoordinates = async (cityName) => {
+        const url = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(cityName)}&count=1&language=pt&format=json`
+
+        const response = await fetch(url)
+        const data = await response.json()
+
+        if (!data.results || data.results.length === 0) {
+        throw new Error('Cidade não encontrada')
+        }
+
+        const city = data.results[0]
+
+        return {
+        latitude: city.latitude,
+        longitude: city.longitude,
+        name: city.name,
+        country: city.country
+        }
+    } 
+    
+    const search = async (cityName) => {
+      try{
+        // 1. Buscar Coordenadas
+        const coordinates = await getCoordinates(cityName)
+
+        console.log("Coordenadas:")
+        console.log(coordinates)
+
+        // 2. Pegar a Latitude e Longitude
+        const { latitude, longitude} = coordinates
+
+        // 3. Montar URL do Clima
+        const url = `
+          https://api.open-meteo.com/v1/forecast
+          ?latitude=${latitude}
+          &longitude=${longitude}
+          &current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code
+        `.replace(/\s/g, '')
+
+        // 4. Buscar clima
+        const response = await fetch(url)
+        const data =  await response  .json()
+
+        console.log('Clima:')
+        console.log(data)
+
+      } catch (error) {
+      console.error(error.message)
+    }
+    }
+
+    const teste = search('London')
+
+
   return (
     <div className="container">
       <div className="weather-app">
